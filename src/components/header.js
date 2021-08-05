c579e6505520d09e7f35f55a1c251fa9c8b01b64
Waffi503 <carlosbrandan503@gmail.com>
2021-08-05 12:55:36 -0300

    haciendo el header responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,29 @@
 import React,{ Component } from 'react';
 import '../styles/header.css';
+import { FaBars } from 'react-icons/fa';
+import { Link } from 'wouter'
 export default class Header extends Component{
-
+   
     render(){
+        function showmenu(){
+           
+            document.getElementById("menu-items").classList.toggle("show");
+    
+        }
+
         return(
-            
-            <ul className="Menu-ul">
-                <li className="logo">Axel Cervezeria</li>
-                <li className="elemnt">Productos</li>
-                <li className="elemnt">Contacto</li>
-                <li className="elemnt">Informacion</li>
-            </ul>
-          
+            <div className="menu">
+                <h1 className="logo">Axel Cervezeria</h1>
+                <ul id="menu-items" className="Menu-ul">
+                    <li><Link className="activado" href="#">Inicio</Link></li>
+                    <li><Link className="no-activado" href="#">Productos</Link></li>
+                    <li><Link className="no-activado" href="#">Contacto</Link></li>
+                    <li><Link className="no-activado" href="#">Informacion</Link></li>
+                </ul>
+                <span className="btn-menu" onClick={showmenu}>
+                    <FaBars/>
+                </span>
+            </div>
             
 
         )
