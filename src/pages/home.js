75854413aa42b63cfb5557f79dff9fe4ae985ed0
Waffi503 <carlosbrandan503@gmail.com>
2021-08-05 11:01:25 -0300

    agregando otra grid en la home y estilizando la home

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,13 +6,26 @@ return (<>
     <Header/>
 
 <div className="container">
-    <div className="container-desc">
-        <h1>Cerveza tal</h1>
-        <p>La elaboración de una cerveza artesanal, nace de la idea de crear algo a menor escala, de forma “manual” para conservar al máximo el control sobre la elaboración, permitiendo hacer pequeños cambios que influirán en el gusto que dará al paladar “menos sofisticado” o “experimentado”.</p>
+
+    <div className="contenedor-grid color-beige">
+
+        <div className="container-desc">
+            <h1>Cerveza tal</h1>
+            <p>La elaboración de una cerveza artesanal, nace de la idea de crear algo a menor escala, de forma “manual” para conservar al máximo el control sobre la elaboración, permitiendo hacer pequeños cambios que influirán en el gusto que dará al paladar “menos sofisticado” o “experimentado”.</p>
+        </div>
+        <img src="https://i.imgur.com/bllrwTf.png" alt="cerveza" className="container-img"/>
     </div>
 
-    <img src="https://i.imgur.com/bllrwTf.png" alt="cerveza" className="container-img"/>
+    <div className="contenedor-grid">  
+        <img src="https://i.imgur.com/bllrwTf.png" alt="cerveza" className="container-img"/>
+        <div className="container-desc">
+            <h1>Cerveza tal</h1>
+            <p>La elaboración de una cerveza artesanal, nace de la idea de crear algo a menor escala, de forma “manual” para conservar al máximo el control sobre la elaboración, permitiendo hacer pequeños cambios que influirán en el gusto que dará al paladar “menos sofisticado” o “experimentado”.</p>
+        </div>
+
+    </div>
 </div>
+
 
 
 
