f432b3b0c466f9fa0d73bbd00f6a753fbe0f4d18
Waffi503 <carlosbrandan503@gmail.com>
2021-08-08 10:24:26 -0300

    Asignando nuevos recursos

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,12 @@ export default function Home(){
     return(
 <><Header productos="disabled" informacion="disabled" inicio="enable" contacto="disabled"/>
         
-<div className="container"></div>
+<div className="container">
+        <div className="hero">
+            <img alt="logo" className="logo-hero" src="https://i.imgur.com/0nY2hco.png"/>
+            <h1 className="sub-titulo">BIWALAX</h1>
+        </div>
+</div>
 
 </>
     )
--- src/pages/product.js
+++ src/pages/product.js
@@ -9,18 +9,26 @@ return (<>
 
     <div className="contenedor-grid color-beige">
         <div className="container-desc">
-            <h1>Cerveza tal</h1>
+            <h1>Dorada Pampeana</h1>
             <p>La elaboración de una cerveza artesanal, nace de la idea de crear algo a menor escala, de forma “manual” para conservar al máximo el control sobre la elaboración, permitiendo hacer pequeños cambios que influirán en el gusto que dará al paladar “menos sofisticado” o “experimentado”.</p>
         </div>
-        <img src="https://i.imgur.com/bllrwTf.png" alt="cerveza" className="container-img"/>
+        <img src="https://i.imgur.com/XOabvtI.png" alt="cerveza" className="container-img"/>
     </div>
 
     <div className="contenedor-grid">  
-        <img src="https://i.imgur.com/bllrwTf.png" alt="cerveza" className="container-img"/>
+        <img src="https://i.imgur.com/G0qA6V3.png" alt="cerveza" className="container-img"/>
         <div className="container-desc">
-            <h1>Cerveza tal</h1>
+            <h1>Honey Beer</h1>
             <p>La elaboración de una cerveza artesanal, nace de la idea de crear algo a menor escala, de forma “manual” para conservar al máximo el control sobre la elaboración, permitiendo hacer pequeños cambios que influirán en el gusto que dará al paladar “menos sofisticado” o “experimentado”.</p>
         </div>
+    </div>
+
+    <div className="contenedor-grid color-beige">
+        <div className="container-desc">
+            <h1>Scottish Ale Export</h1>
+            <p>La elaboración de una cerveza artesanal, nace de la idea de crear algo a menor escala, de forma “manual” para conservar al máximo el control sobre la elaboración, permitiendo hacer pequeños cambios que influirán en el gusto que dará al paladar “menos sofisticado” o “experimentado”.</p>
+        </div>
+        <img src="https://i.imgur.com/7UFhjqq.png" alt="cerveza" className="container-img"/>
     </div>
 </div>
 
