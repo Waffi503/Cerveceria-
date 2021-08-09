a08b96fb2f3a87e7af9a38bc531df0021e6382bb
Waffi503 <carlosbrandan503@gmail.com>
2021-08-08 22:47:34 -0300

    cambiando de grid a flexbox productos

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,7 @@ export default function Home(){
     return(
 <><Header productos="disabled" informacion="disabled" inicio="enable" contacto="disabled"/>
         
-<div className="container">
+<div className="container-hero">
         <div className="hero">
          <img alt="logo" className="logo-hero" src="https://i.imgur.com/0nY2hco.png"/>
         </div>
--- src/pages/product.js
+++ src/pages/product.js
@@ -10,9 +10,13 @@ return (<>
     <div className="contenedor-grid color-beige">
         <div className="container-desc">
             <h1>Dorada Pampeana</h1>
-            <p>La elaboración de una cerveza artesanal, nace de la idea de crear algo a menor escala, de forma “manual” para conservar al máximo el control sobre la elaboración, permitiendo hacer pequeños cambios que influirán en el gusto que dará al paladar “menos sofisticado” o “experimentado”.</p>
+            <p>La versión Argentina del histórico estilo inglés, ya es un estilo reconocido mundialmente. Se diferencia de la IPA Americana por agregado de trigo a la receta de granos y el uso de lúpulos Argentinos que tienen características únicas de sabor y aroma. Se busca que las caracterísiticas cítricas del lúpulo Argentino armonicen con el trigo, como sucede en la Witbier. El agregado de bajas cantidades de trigo puede recordar al grist de la Kolsch, donde también hay un frutado producto de la fermentación. 
+
+Estamos ante una Pale Ale orgullosamente Argentina decididamente amarga y lupulada, refrescante y moderadamente fuerte. La clave del estilo está en la tomabilidad sin asperezas y con un buen balance.</p>
         </div>
-        <img src="https://i.imgur.com/XOabvtI.png" alt="cerveza" className="container-img"/>
+        <div className="container-img">
+        <img src="https://i.imgur.com/XOabvtI.png" alt="cerveza" className="img-product"/>
+        </div>
     </div>
 
     <div className="contenedor-grid">  
